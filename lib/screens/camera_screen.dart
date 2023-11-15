fa5994d7d5f135f2969b472d2f40d20531055f89
Aaron Sutton <hey@aaron.as>
2023-11-15 02:27:48 -0500

    Scale for full screen

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -6,16 +6,15 @@ import 'package:flutter/material.dart';
 
 /// A widget that manages using the camera.
 class CameraScreen extends StatefulWidget {
-
   // Constructor requires item name, image URL, and category.
   const CameraScreen({
     Key? key,
-		required this.camera,
+    required this.camera,
   }) : super(key: key);
 
-	final CameraDescription? camera;
+  final CameraDescription? camera;
 
-	@override
+  @override
   CameraScreenState createState() => CameraScreenState();
 }
 
@@ -27,21 +26,20 @@ class CameraScreenState extends State<CameraScreen> {
   void initState() {
     super.initState();
 
-		final camera = widget.camera;
+    final camera = widget.camera;
 
-		if (camera != null) {
-			_controller = CameraController(
-				// Get a specific camera from the list of available cameras.
-				camera,
-				// Define the resolution to use.
-				ResolutionPreset.medium,
-			);
+    if (camera != null) {
+      _controller = CameraController(
+        // Get a specific camera from the list of available cameras.
+        camera,
+        // Define the resolution to use.
+        ResolutionPreset.medium,
+      );
+    } else {
+      _controller = null;
+    }
 
-		} else {
-			_controller = null;
-		}
-
-		_initializeControllerFuture = _controller?.initialize() ?? Future.value();
+    _initializeControllerFuture = _controller?.initialize() ?? Future.value();
   }
 
   @override
@@ -53,21 +51,41 @@ class CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-		final controller = _controller;
-		return Scaffold(
-			body: FutureBuilder<void>(
-				future: _initializeControllerFuture,
-				builder: (context, snapshot) {
-					if (controller == null) {
-						return const Center(child: Icon(Icons.videocam_off));
-					} else if (snapshot.connectionState == ConnectionState.done) {
-						return CameraPreview(controller);
-					} else {
-						return const Center(child: CircularProgressIndicator());  
-					}
-				}
-			)
-		);
+    final controller = _controller;
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+        body: FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (controller == null) {
+                return const Center(child: Icon(Icons.videocam_off));
+              } else if (snapshot.connectionState == ConnectionState.done) {
+                return ClipRect(
+                  clipper: _MediaSizeClipper(size),
+                  child: Transform.scale(
+                    scale: 1 / (controller.value.aspectRatio * size.aspectRatio),
+                    alignment: Alignment.topCenter,
+                    child: CameraPreview(controller),
+                  ),
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }));
+  }
+}
+
+class _MediaSizeClipper extends CustomClipper<Rect> {
+  final Size mediaSize;
+  const _MediaSizeClipper(this.mediaSize);
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 0, mediaSize.width, mediaSize.height);
   }
 
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
 }
