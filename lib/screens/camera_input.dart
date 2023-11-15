3c3112cce0dfc0343e4feafa7148023559bdefcd
Aaron Sutton <hey@aaron.as>
2023-11-15 00:29:52 -0500

    Add iOS camera entitlement keys

---
+++ lib/screens/camera_input.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+/// A widget that manages using the camera.
+class CameraInput extends StatelessWidget {
+
+  // Constructor requires item name, image URL, and category.
+  const CameraInput({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
