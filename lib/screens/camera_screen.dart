b9508d036aca66cf576b911ecb687e4f31177c2c
Aaron Sutton <hey@aaron.as>
2023-11-15 02:11:47 -0500

    Support nullable camera
    Used when running in an environment without a camera, such
    as the simulator.

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -13,43 +13,61 @@ class CameraScreen extends StatefulWidget {
 		required this.camera,
   }) : super(key: key);
 
-	final CameraDescription camera;
+	final CameraDescription? camera;
 
 	@override
   CameraScreenState createState() => CameraScreenState();
 }
 
 class CameraScreenState extends State<CameraScreen> {
-  late CameraController _controller;
+  late CameraController? _controller;
   late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
     super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
-    _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
-      widget.camera,
-      // Define the resolution to use.
-      ResolutionPreset.medium,
-    );
 
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller.initialize();
+		final camera = widget.camera;
+
+		if (camera != null) {
+			_controller = CameraController(
+				// Get a specific camera from the list of available cameras.
+				camera,
+				// Define the resolution to use.
+				ResolutionPreset.medium,
+			);
+
+		} else {
+			_controller = null;
+		}
+
+		_initializeControllerFuture = _controller?.initialize() ?? Future.value();
   }
 
   @override
   void dispose() {
     // Dispose of the controller when the widget is disposed.
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // Fill this out in the next steps.
-    return Container();
+		final controller = _controller;
+		return Scaffold(
+			body: FutureBuilder<void>(
+				future: _initializeControllerFuture,
+				builder: (context, snapshot) {
+					if (controller == null) {
+						return const Center(child: Icon(Icons.videocam_off));
+					} else if (snapshot.connectionState == ConnectionState.done) {
+						return CameraPreview(controller);
+					} else {
+						return const Center(child: CircularProgressIndicator());  
+					}
+				}
+			)
+		);
   }
 
 }
