2c4f2dbb1c1dc7750b1412adbb96660e680b738f
Zipan Huang <yuh115@pitt.edu>
2023-12-13 14:35:36 -0500

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,9 +12,17 @@ import '../screens/camera_screen.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen();
 
-  @override
+@override
   Widget build(BuildContext context) {
     const horizontalPadding = 16.0;
+
+    // fake data
+    final String verticalImagePath = 'assets/images/cj.jpeg';
+    final List<String> squareImagePaths = [
+      'assets/images/hat.jpeg',
+      'assets/images/sungass.jpeg',
+      'assets/images/boot.jpeg',
+    ];
 
     return Scaffold(
       appBar: AppBar(
@@ -24,23 +32,63 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          // using  SingleChildScrollView including  Column
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                child: const GreetingSection(username: 'Aaron'), // greeting
+                child: const GreetingSection(username: 'Aaron'),
               ),
-              const SizedBox(height: 16), // add some space
+              const SizedBox(height: 16),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                child: const WeatherInfoSection(), // weather
+                child: const WeatherInfoSection(),
               ),
+              // Padding(
+              //   padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+              //   child: const UserAvatar(), // suggestion avatar
+              // ),
+
+              // add suggestion 
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
-                child: const UserAvatar(), // suggestion avatar
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start, 
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 300, 
+                        margin: EdgeInsets.only(right: 10), 
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: DecorationImage(
+                            image: AssetImage(verticalImagePath),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    // right
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: squareImagePaths.map((path) => Container(
+                          height: 100, 
+                          margin: EdgeInsets.only(bottom: 10), 
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            image: DecorationImage(
+                              image: AssetImage(path),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )).toList(),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
