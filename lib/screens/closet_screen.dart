d0ec5bd201bd2c601c3b19f825c780d2676c4fba
Zipan Huang <yuh115@pitt.edu>
2023-12-13 13:37:57 -0500

    Update closet_screen.dart

--- lib/screens/closet_screen.dart
+++ lib/screens/closet_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import '../models/clothing.dart';
-import '../widgets/clothing_list_tile.dart';
-import '../delegates/closet_search_delegate.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'add_clothes_screen.dart';
 import 'outfit_creation_screen.dart';
-import 'category_detail_screen.dart'; 
+import 'category_detail_screen.dart'; // Ensure you have this screen for showing category details
 
 class ClosetScreen extends StatefulWidget {
   ClosetScreen({Key? key}) : super(key: key);
@@ -14,77 +12,67 @@ class ClosetScreen extends StatefulWidget {
 }
 
 class _ClosetScreenState extends State<ClosetScreen> {
-  int _selectedIndex = 2;
 
-  // Navigate to AddClothesScreen to add new clothes
   void _navigateToAddClothesScreen() {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (context) => AddClothesScreen()),
     );
   }
 
-  // Navigate to OutfitCreationScreen to create a new outfit
   void _navigateToOutfitCreationScreen() {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (context) => OutfitCreationScreen()),
     );
   }
 
-  // Navigate to CategoryDetailScreen when a category is tapped
   void _navigateToCategoryDetail(String category) {
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => CategoryDetailScreen(category: category, clothes: [],),
+        builder: (context) => CategoryDetailScreen(category: category, clothes: []),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    final Map<String, List<Clothing>> categories = {
-      'Outfits': [],
-      'Tops': [],
-      'Bottoms': [],
-      'Shoes': [],
-      'Accessories': [],
-      // Add other categories as needed
-    };
+    final List<String> categories = ['Outfits', 'Tops', 'Bottoms', 'Shoes', 'Accessories'];
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Closet'),
-        automaticallyImplyLeading: false, // Do not show the back button
+        title: Text(
+          'Your Closet',
+          style: GoogleFonts.nunito(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
         actions: [
           IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: _navigateToAddClothesScreen, // Navigate to the add clothing page
+            icon: const Icon(Icons.add, color: Colors.black),
+            onPressed: _navigateToAddClothesScreen,
           ),
           IconButton(
-            icon: const Icon(Icons.camera),
-            onPressed: _navigateToOutfitCreationScreen, // Navigate to the outfit creation page
+            icon: const Icon(Icons.share, color: Colors.black),
+            onPressed: _navigateToOutfitCreationScreen,
           ),
-          // IconButton(
-          //   icon: const Icon(Icons.search),
-          //   onPressed: () {
-          //     showSearch(
-          //       context: context,
-          //       delegate: ClosetSearchDelegate(categories.keys.cast<Clothing>().toList(), categories: []), // Pass the categories
-          //     );
-          //   },
-          // ),
         ],
       ),
-      body: ListView.builder(
+      body: ListView.separated(
         itemCount: categories.length,
+        separatorBuilder: (context, index) => Divider(color: Colors.grey[300]),
         itemBuilder: (context, index) {
-          String category = categories.keys.elementAt(index);
-          return ClothingListTile(
-            category: category,
-            clothes: categories[category]!,
-              onTap: () {
-                          _navigateToCategoryDetail(category);
-                        },
-  // Handle tap here
+          return ListTile(
+            title: Text(
+              categories[index],
+              style: GoogleFonts.nunito(fontSize: 18, color: Colors.black),
+            ),
+            trailing: Icon(Icons.chevron_right, color: Colors.grey),
+            onTap: () {
+              _navigateToCategoryDetail(categories[index]);
+            },
           );
         },
       ),
