493f3df7978d2e7dc8581025dbadc016f0d3b33a
JessDang <jesstdang@gmail.com>
2023-12-13 03:48:59 -0500

    changes to homescreen

--- lib/widgets/user_avatar.dart
+++ lib/widgets/user_avatar.dart
@@ -1,1 +1,20 @@
 // A widget for displaying the user's avatar. not really need ?
+
+import 'package:flutter/material.dart';
+
+class UserAvatar extends StatelessWidget {
+  const UserAvatar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.symmetric(vertical: 12),
+      height: 400, // Adjust the height as needed
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 185, 214, 185),
+        borderRadius: BorderRadius.circular(25),
+      ),
+    );
+  }
+}
